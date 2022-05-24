dc4aac1b37933d2b248196408b62b34e7c5a8cdd
amirameri93 <100429387+amirameri93@users.noreply.github.com>
2022-05-24 12:33:02 +0430

    Initial commit

---
+++ zigma.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+ {
+ 	int number, x;
+ 	cout<<"please enter the number:"<<endl;
+ 	cin>>number;
+ 	for(int i=1;i<=number;i++)
+ 	{
+ 		x=x+i;
+	 }
+ 	cout<<"zigma= "<<x;
+	return 0;
+}
